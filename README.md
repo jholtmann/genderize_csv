8630a789b6ceda878b0886c6ce029c51f5764c49
Jonathan Holtmann <contact@jonathanholtmann.com>
2017-07-26 14:29:47 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# genderize_csv
-Python script to determine genders of names in csv file
+# Genderize Python
+Python genderize.io script
+
+```sh
+python genderize.py [input csv] [output csv]
+```
+
+This script takes a single column CSV file with a header (first row says "name" or other) and feeds the names to genderize.io. It outputs a CSV file with the name, gender, probability, and count of every name.
+
+##### Features:
+- Bulk processing (tested up to 100,000 names).
+- Estimates remaining time.
+- Writes data after processing 10 names so little data is lost if genderize.io responds with a 502 error, network connection is lost, or request limit is reached.
+- Support for genderize.io API key (allows processing of more than 1000 names /mo).
+
+#### Dependencies:
+- https://pypi.python.org/pypi/Genderize (https://github.com/SteelPangolin/genderize)
+
+#### To-do:
+- ~~Catch 502 bad gateway error and retry the request. Currently the program will just catch the error, print it, and exit.~~ DONE (not tested thouroughly enough yet, so genderize_nerrc.py was added in case error catching causes problems)
+- Add ability to search multi-column CSV file for column with specific header.
+- Add support for optionally caching gender responses and searching through them for identical names before asking genderize for the data. This would lower API key request usage.
+
+##### Note:
+For some reason, the python genderize client used limits requests to 10 names. To work around this, the code breaks the names down into chunks of 10. This has the unintentional benefit of preventing data loss in case of a crash/server error as the results are written every 10 names. As such, it doesn't seem worth looking into why the 10 name request limit exists, as it is currenty also a feature.
---
+++ genderize_nerrc.py
@@ -0,0 +1,83 @@
+from genderize import Genderize, GenderizeException
+import csv
+import sys
+import os.path
+import time
+
+import jpyhelper as jpyh
+
+def genderize():
+    if len(sys.argv) != 3:
+        print("Plrease specify input and output files: python genderize.py [input file path] [output file path]")
+        sys.exit();
+
+    dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    filename, file_extension = os.path.splitext(sys.argv[2])
+    ofile = filename + "_" + time.strftime("%Y%m%d-%H%M%S") + ".csv"
+    print("\n--- Input file: " + dir_path + os.sep + sys.argv[1])
+    print("--- Output file: " + dir_path + os.sep + ofile + "\n")
+
+    csv.field_size_limit(sys.maxsize)
+
+    #Initialize API key
+    genderize = Genderize(
+        user_agent='GenderizeDocs/0.0',
+        api_key='')
+
+    with open(sys.argv[1], encoding="utf8") as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        names = []
+        for row in readCSV:
+            names.append(row)
+
+        names.pop(0)
+
+        print("--- Read CSV with " + str(len(names)) + " names")
+
+        chunks = list(jpyh.splitlist(names, 10));
+
+        print("--- Processed into " + str(len(chunks)) + " chunks")
+
+        if jpyh.query_yes_no("\n---! Ready to send to Genderdize. Proceed?") == False:
+            print("Exiting...\n")
+            sys.exit()
+
+        if os.path.isfile(ofile):
+            if jpyh.query_yes_no("---! Output file exists, overwrite?") == False:
+                print("Exiting...\n")
+                sys.exit()
+            print("\n")
+
+        response_time = [];
+        with open(ofile, 'w', newline='', encoding="utf8") as f:
+            writer = csv.writer(f)
+            writer.writerow(list(["names", "gender", "probability", "count"]))
+            chunks_len = len(chunks)
+            for index, chunk in enumerate(chunks):
+                start = time.time()
+                #while True:
+                try:
+                    dataset = genderize.get(chunk)
+                    #break
+                except GenderizeException as e:
+                    print(e)
+                    '''
+                    if "response not in JSON format" in e:
+                        if jpyh.query_yes_no("\n---! 502 detected, try again?") == True:
+                            print("Exiting...\n")
+                            pass
+                        else:
+                            break
+                    '''
+                    print("\n---!! GenderizeException - You probably exceeded the request limit, please add or purchase a API key\n")
+                    sys.exit()
+
+                response_time.append(time.time() - start)
+                print("Processed chunk " + str(index + 1) + " of " + str(chunks_len) + " -- Time remaining (est.): " + \
+                    str( round( (sum(response_time) / len(response_time) * (chunks_len - index - 1)), 3)) + "s")
+
+                for data in dataset:
+                    writer.writerow(data.values())
+
+if __name__ == "__main__":
+    genderize()
