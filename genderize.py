815ba5886e11d12b248fdc7791f24d5eb6f5af51
Jonathan Holtmann <jholtmann@users.noreply.github.com>
2017-07-28 11:54:50 -0400

    Added command line flags, basic error handling

--- genderize.py
+++ genderize.py
@@ -3,39 +3,67 @@ import csv
 import sys
 import os.path
 import time
+import argparse
+import logging
 
 import jpyhelper as jpyh
 
-def genderize():
-    if len(sys.argv) < 3:
-        print("Plrease specify input and output files: python genderize.py [input file path] [output file path]")
-        sys.exit();
+def genderize(args):
+    print(args)
 
-    if len(sys.argv) == 4:
-        API_KEY = sys.argv[3]
+    #File initialization
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    logging.basicConfig(filename=dir_path + os.sep + "log.txt", level=logging.DEBUG,
+                        format='%(asctime)s %(levelname)s %(name)s %(message)s')
+    logger=logging.getLogger(__name__)
+
+    ofilename, ofile_extension = os.path.splitext(args.output)
+
+    ofile = ofilename + "_" + time.strftime("%Y%m%d-%H%M%S") + ".csv"
+    ifile = args.input
+
+    if os.path.isabs(ifile):
+        print("\n--- Input file: " + ifile)
     else:
-        API_KEY = ""
+        print("\n--- Input file: " + dir_path + os.sep + ifile)
 
-    dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    filename, file_extension = os.path.splitext(sys.argv[2])
-    ofile = filename + "_" + time.strftime("%Y%m%d-%H%M%S") + ".csv"
-    print("\n--- Input file: " + dir_path + os.sep + sys.argv[1])
-    print("--- Output file: " + dir_path + os.sep + ofile + "\n")
+    if os.path.isabs(ofile):
+        print("--- Output file: " + ofile)
+    else:
+        print("--- Output file: " + dir_path + os.sep + ofile + "\n")
 
+    #File integruty checking
+    if not os.path.exists(ifile):
+        print("--- Input file does not exist. Exiting.\n")
+        sys.exit()
+
+    if not os.path.exists(os.path.dirname(ofile)):
+        print("--- Error! Invalid output file path. Exiting.\n")
+        sys.exit()
+
+    #Some set up stuff
     csv.field_size_limit(sys.maxsize)
 
     #Initialize API key
-    genderize = Genderize(
-        user_agent='GenderizeDocs/0.0',
-        api_key=API_KEY)
+    if not args.key == "NO_API":
+        print("--- API key: " + args.key + "\n")
+        genderize = Genderize(
+            user_agent='GenderizeDocs/0.0',
+            api_key=args.key)
+        key_present = True
+    else:
+        print("--- No API key provided.\n")
+        key_present = False
 
-    with open(sys.argv[1], 'r', encoding="utf8") as csvfile:
+    #Open ifile
+    with open(ifile, 'r', encoding="utf8") as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         names = []
-        for row in readCSV:
+        for row in readCSV: #Read CSV into names list
             names.append(row)
 
-        names.pop(0)
+        names.pop(0) #Remove header
 
         print("--- Read CSV with " + str(len(names)) + " names")
 
@@ -65,17 +93,26 @@ def genderize():
                         temp = []
                         for c in chunk:
                             temp.append(str(c[0]).strip("\n"))
-                        dataset = genderize.get(temp)
+                        if key_present:
+                            dataset = genderize.get(temp)
+                        else:
+                            dataset = Genderize().get(temp)
                     except GenderizeException as e:
-                        print("\n" + str(e))
+                        #print("\n" + str(e))
+                        logger.error(e)
 
-                        if "response not in JSON format" in str(e):
-                            if jpyh.query_yes_no("\n---! 502 detected, try again?") == True:
+                        #Error handling
+                        if "response not in JSON format" in str(e) and args.catch == True:
+                            if jpyh.query_yes_no("\n---!! 502 detected, try again?") == True:
                                 print("Exiting...\n")
                                 pass
                             else:
                                 break
-                        print("\n---!! GenderizeException - You probably exceeded the request limit, please add or purchase a API key\n")
+                        if "Invalid API key" in str(e) and args.catch == True:
+                            print("\n---!! Error, invalid API key! Check log file for details.\n")
+                            sys.exit()
+
+                        print("\n---!! GenderizeException - You probably exceeded the request limit, please add or purchase a API key. Check log file for details.\n")
                         sys.exit()
 
                     response_time.append(time.time() - start)
@@ -87,4 +124,12 @@ def genderize():
                     break
 
 if __name__ == "__main__":
-    genderize()
+    parser = argparse.ArgumentParser(description='Bulk genderize.io script')
+    required = parser.add_argument_group('required named arguments')
+
+    required.add_argument('-i','--input', help='Input file name', required=True)
+    required.add_argument('-o','--output', help='Output file name', required=True)
+    parser.add_argument('-k','--key', help='API key', required=False, default="NO_API")
+    parser.add_argument('-c','--catch', help='Try to gracefully handle server 502 errors', required=False, action='store_true', default=True)
+
+    genderize(parser.parse_args())
